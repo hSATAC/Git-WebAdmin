bae215387692d1dcbbf708624ac6968602393f9f
hSATAC <hsatac@gmail.com>
2011-10-17 18:01:30 +0800

    repo list

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ require 'Slim/Slim.php';
 $app = new Slim();
 
 $app->get('/', function() use($app) {
-  $app->render('index.tpl.php');
+  // 取得所有 repo
+  $repo_list = array_filter(glob('../*.git'), 'is_dir');
+  $vars = compact('repo_list');
+  $app->render('index.tpl.php', $vars);
 });
 
 $app->run();
--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,1 +1,15 @@
-test
+<html>
+<head>
+</head>
+<body>
+<h1>Git WebAdmin</h1>
+
+<h2>Repositories List:</h2>
+<ul>
+<?foreach($repo_list as $repo): ?>
+  <li><a target="_blank" href="/<?=substr($repo,3)?>"><?= substr($repo,3) ?></a></li>
+<?endforeach;?>
+</ul>
+<h2>Create New Repo:</h2>
+</body>
+</html>
